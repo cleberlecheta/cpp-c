caf011db216d38b2814657db4d8a12cd569cc72d
Jesus <cleber@encontreiJesus.com>
2021-11-27 12:03:10 -0300

    #72 - Biblioteca iostream

--- libraries/72-iostream.cpp
+++ libraries/72-iostream.cpp
@@ -1,4 +1,9 @@
-#include <iostream>
+#include <iostream> 
+/** comandos de entrada e saída básicos 
+ * - cin
+ * - cout
+ * - 
+*/
 
 using namespace std;
 
@@ -9,13 +14,13 @@ int main() {
     cin >> nome >> idade;
     cout << nome << " - " << idade << endl;
 
-    char nome2[80];
+    char nome2[80]; //ou char *nome2;
     cin.get(nome2, 80); //le caractere por caractere, leitura de arquivo melhor usar getline()
     cout << nome2 << " - " << cin.gcount() << endl;
 
     char n,s;
     n = cin.get(); //primeira letra do nome
-    cin.ignore(80, ' ');
+    cin.ignore(80, ' '); //ignorar pontos, epaços ou qualquer outro caractere
     s=cin.get(); //primeira letra do sobrenome
     cout << n << " - " << s << endl;
 
