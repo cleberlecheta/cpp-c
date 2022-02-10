b26e7c6a1d64dc290509c32db1df78e50b2f7679
Jesus <cleber@encontreiJesus.com>
2022-02-10 18:03:24 -0300

    Biblioteca iomanip - Curso de C++ #101 Biblioteca mágica bits/stdc++.h - Curso de C++ #102 -- fim do estudo das bibliotecas --

--- libraries/101-iomanip.cpp
+++ libraries/101-iomanip.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-/* Biblioteca cmath
- * https://www.youtube.com/watch?v=bAOUB9b4My4
+/* Biblioteca iomanip - manipulação de sáida
+ * https://www.youtube.com/watch?v=vNbWKn5maDU
  */
 int main() {
   
+  //setbase = base: decimal, hexadecimal, octal
+  cout << setbase(16);
+  cout << "10 em base 16: " << 10 << endl;
+  cout << setbase(10);
+  cout << "10 em base decimal: " << 10 << endl << endl;
+
+  //setw = largura do campo
+  cout << setw(20);
+  cout << "Jesus";
+  cout << setw(10);
+  cout << "te";
+  cout << setw(40);
+  cout << "ama++" << endl << endl;
+
+  //setfill = Preenchimento de campo
+  cout << "Jesus " << setw(20) << setfill('.') << " o caminho, a verdade e a vida!" << endl << endl;
+
+  //setprecision = Precisão de valores de float e double, casas decimais
+  double pi = 3.14159;
+  cout << setprecision(3) << pi << endl << endl;
+
   return 0;
 }
--- libraries/102-bits-stdc++.cpp
+++ libraries/102-bits-stdc++.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-/* Biblioteca cmath
- * https://www.youtube.com/watch?v=bAOUB9b4My4
+/* Biblioteca bits/stdc++.h - um include que inclui todas as bibliotecas padrões
+ * https://www.youtube.com/watch?v=BAH89SZU_O4
  */
 int main() {
   
+  //bibliteca utility (sem include)
+  pair<int, int> par;
+  par = make_pair(10, 5);
+  cout << par.first << " - " << par.second << endl << endl;
+
+  //biblioteca vector (sem include)
+  vector<int> vt = {1,3,5,7,9};
+  for (auto x: vt) {
+    cout << x << " - ";
+  }
+  cout << endl << endl;
+
+  //biblioteca cmatch (sem include)
+  double x = 10;
+  cout << pow(x, 2) << endl;
+  
+  fstream f;
+  f.open("102-bits.txt", fstream::in|fstream::out|fstream::app);
+  f << "Siga o caminho!" << endl;
+  f.close();
+
   return 0;
 }
